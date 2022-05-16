bd2c16a85703278981e7a6b0f42c7e35760cfc6d
Андрей <andrei20012010@mial.ru>
2022-05-16 21:37:45 +0300

    Добавил вывод времени и защиту от отр. разб.

--- WpfApp1/Classes/TrapezoidCalculator.cs
+++ WpfApp1/Classes/TrapezoidCalculator.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace WpfApp1.Classes
 {
@@ -9,15 +12,23 @@ namespace WpfApp1.Classes
 
         public double Calculate(int splitCount, double upLim, double lowLim, Func<double, double> integral, out double time)
         {
-            time = 0;
-            double h = (upLim - lowLim) / (double)splitCount; if (splitCount < 0) { throw new ArgumentException("count < 0"); }
+            if (splitCount <= 0)
+            {
+                throw new ArgumentException();
+            }
+            Stopwatch stopWatch = new Stopwatch();
+            
+            double h = (upLim - lowLim) / (double)splitCount; 
             double sum = 0;
-
+            stopWatch.Start();
             for (int i = 1; i < splitCount; i++)
             {
                 sum += integral(lowLim + h * i); 
             }
             sum += (integral(lowLim) + integral(upLim)) / 2;
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            time = ts.TotalMilliseconds;
 
             return h * sum;
         }
